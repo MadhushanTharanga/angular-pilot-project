ed02a4fcb8e465000782545217dcb4e70c305718
MadhushanTharanga <madhushan0617@gmail.com>
2025-02-21 09:34:42 +0530

    updated!

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import {CommentsService} from "../../services/comments.service";
 import {response} from "express";
+import {CommonModule} from "@angular/common";
+import {HttpClientModule} from "@angular/common/http";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,HttpClientModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent {
+  comments : any[]=[];
   constructor(private _service:CommentsService) {
   }
   load(){
@@ -17,6 +20,7 @@ export class HomePageComponent {
   }
   loadAllData(){
     this._service.findAllComments().subscribe(response=>{
+      this.comments=response;
       console.log(response)
     })
   }
--- src/app/services/comments.service.ts
+++ src/app/services/comments.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {environment} from "../../environments/environment.development";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentsService {
 
-  baseUrl = environment.url;
+  private baseUrl = environment.url;
   constructor(private _http:HttpClient) { }
 
   findAllComments():Observable<any>{
