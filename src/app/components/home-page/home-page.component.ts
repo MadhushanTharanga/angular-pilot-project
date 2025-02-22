8aba161670d1b37085e58d29b862e1e165691a06
MadhushanTharanga <madhushan0617@gmail.com>
2025-02-22 12:44:26 +0530

    updated!

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CommentsService} from "../../services/comments.service";
 import {response} from "express";
 import {CommonModule} from "@angular/common";
 import {HttpClientModule} from "@angular/common/http";
+import {FormControl, ReactiveFormsModule} from "@angular/forms";
+import {debounceTime} from "rxjs";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule,HttpClientModule],
+  imports: [CommonModule, HttpClientModule, ReactiveFormsModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit{
+  searchText=new FormControl();
   comments : any[]=[];
   constructor(private _service:CommentsService) {
   }
@@ -22,6 +25,14 @@ export class HomePageComponent {
     this._service.findAllComments().subscribe(response=>{
       this.comments=response;
       console.log(response)
+    })
+  }
+
+  ngOnInit(): void {
+    this.searchText.valueChanges.pipe(
+      debounceTime(500)
+    ).subscribe(value=>{
+      console.log(`[${value}]`)
     })
   }
 }
